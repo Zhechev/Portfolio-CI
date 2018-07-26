e9cab7e5d1f7ed4d8a8cee63a13b9df8225c4cf0
mzhechev <mitko_jechev@mail.bg>
2018-07-26 21:03:33 +0300

    init CI-v3.1.9 and AdminLTE admin template

---
+++ application/controllers/admin/Login.php
@@ -0,0 +1,24 @@
+﻿<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Login extends CI_Controller {
+    public function index()
+    {
+        $this->load->helper('form');
+        $data['formEmail'] = array(
+            'name'          => 'email',
+            'value'         => '',
+            'placeholder'   => 'Email',
+            'class'         => 'form-control'
+        );
+
+        $data['formPassword'] = array(
+            'name'          => 'password',
+            'id'            => 'password',
+            'value'         => '',
+            'placeholder'   => 'Password',
+            'class'         => 'form-control'
+        );
+        $this->load->view('admin/zz', $data);
+    }
+}
